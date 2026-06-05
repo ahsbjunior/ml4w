b6b8387252661e7d6f22a57f56bf0790a7ed1e94
Afonso Junior <ahsbjunior@gmail.comm>
2026-06-05 12:29:50 -0300

    Colorscheme: alterando cor de selecao no vim

--- com.ml4w.dotfiles.stable/.config/nvim/lua/ahsbjunior/plugins/colorscheme.lua
+++ com.ml4w.dotfiles.stable/.config/nvim/lua/ahsbjunior/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
       local bg_dark = "#011423"
       local bg_highlight = "#143652"
       local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
+      local bg_visual = "#7A6923"
       local fg = "#CBE0F0"
       local fg_dark = "#B4D0E9"
       local fg_gutter = "#627E97"
