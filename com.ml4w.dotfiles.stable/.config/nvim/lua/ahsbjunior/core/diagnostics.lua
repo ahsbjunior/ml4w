2aa4c90b5e7c0cc2c999ade403b4191f83ad961c
Afonso Junior <ahsbjunior@gmail.comm>
2026-08-28 16:15:10 -0300

    neovim: adicionando nerdfonts para os diagnósticos

--- com.ml4w.dotfiles.stable/.config/nvim/lua/ahsbjunior/core/diagnostics.lua
+++ com.ml4w.dotfiles.stable/.config/nvim/lua/ahsbjunior/core/diagnostics.lua
@@ -1,3 +1,24 @@
+-- Define os ícones dos diagnostics
+vim.fn.sign_define("DiagnosticSignError", {
+	text = "󰅚",
+	texthl = "DiagnosticSignError",
+})
+
+vim.fn.sign_define("DiagnosticSignWarn", {
+	text = "󰀪",
+	texthl = "DiagnosticSignWarn",
+})
+
+vim.fn.sign_define("DiagnosticSignInfo", {
+	text = "󰋽",
+	texthl = "DiagnosticSignInfo",
+})
+
+vim.fn.sign_define("DiagnosticSignHint", {
+	text = "󰌶",
+	texthl = "DiagnosticSignHint",
+})
+
 vim.diagnostic.config({
 	-- E / W / I / H na coluna lateral
 	signs = true,
